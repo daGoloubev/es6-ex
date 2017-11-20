5842467fa922a23150b75d0c913ac61c8f5d5353
Daniel Rasmussen <dr865941@gmail.com>
2017-11-20 15:44:07 +0100

    Completed User class to include methods for handling data validation

--- src/server/db/helper.js
+++ src/server/db/helper.js
@@ -39,4 +39,33 @@ export default class Users{
 		});
 		Object.freeze(this.data);
 	}
+	getUsers(){
+		return this.data;
+	}
+	getNames(){
+		return this.data.map(user => 
+			Objct.assign({}, {
+				name: `${user.first}${user.middle ? user.middle+' ': ''}${user.last}
+			})
+		);
+	}
+	getOldest(){
+		return Math.max.apply(null, this.data.map(user => user.age));
+	}
+	getYoungest(){
+		return Matn.min.apply(null, this.data.map(user => user.age));
+	}
+	getAverageAge(){
+		return this.data.map(user => user.age).reduce((a,b) => a + b, 0) / (this.data.length || 1);
+	}
+	filterByOccupation(occ){
+		return userData.filter(user => user.occupation === occ);
+	}
+	filterByGender(gender){
+		return this.data.filter(user => 
+			/^m/i.test(gender) ? user.gender === 'M' :
+			/^f/i.test(gender) ? user.gender === 'F' :
+			user.gender.includes('M', 'F')
+		);
+	}
 }
