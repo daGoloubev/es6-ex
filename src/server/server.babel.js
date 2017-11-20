f66104b333cb50ef833cb9f327cc4f056f1694aa
Daniel Rasmussen <dr865941@gmail.com>
2017-11-20 15:05:20 +0100

    Adding more configurations

--- src/server/server.babel.js
+++ src/server/server.babel.js
@@ -1,4 +1,5 @@
+require('dotenv-expand')(require('dotenv').config({silent: true}))
+
 require('babel-polyfill');
 require('babel-register');
 require('./server.js');
-
--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,34 @@
-import express from 'express'
-const app = express();
+import express from 'express';
+import bodyParser from 'body-parser';
+import bunyan from 'bunyan';
+import expressLog from 'express-bunyan-logger';
 
-app.use('/', (req, res, next) => {
-	console.log(req);
-	next();
+const APP = express();
+const LOG = bunyan.createLogger({name: 'server'});
+const ROUTER = express.Router();
+const PORT = process.env.PORT || 8000;
+
+APP.use(expressLog());
+APP.use(expressLog.errorLogger());
+
+APP.use(bodyParser.urlencoded({extended: false}));
+APP.use(bodyParser.json());
+
+
+APP.use('/greeting', (req, res) => {
+	if(req.query.name) {
+		res.json({
+			message: `Hello ${req.query.name}`
+		});
+	} else {
+		res.status(400).send({
+			message: 'What is your name?'
+		});
+	}
 });
 
-app.use('/greeting', (req, res) => {
-	res.json({
-		message: 'Hello'
-	});
-});
+APP.use('/', ROUTER);
 
-app.listen(3000);
+APP.listen(PORT);
+LOG.info(`Express server listening at port ${PORT}`);
+
