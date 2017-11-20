11a645db1443c7300e25124d33306887e628ee2d
Daniel Rasmussen <dr865941@gmail.com>
2017-11-20 16:08:57 +0100

    Implements methods from Model class to send JSON data to response

--- src/server/controllers/myControllers.js
+++ src/server/controllers/myControllers.js
@@ -1,2 +1,40 @@
 import Users from '../db/helper';
 const USERS = new Users();
+
+export default class MyController {
+	users(req, res) {
+		res.json({
+			users: USERS.getUsers()
+		});
+	}
+	names(req, res) {
+		res.json({
+			users: USERS.getNames()
+		});
+	}
+	average(req, res) {
+		res.json({
+			age: USERS.getAverageAge()
+		});
+	}
+	oldest(req, res){
+		res.json({
+			age: USERS.getOldest()
+		});
+	}
+	youngest(req, res){
+		res.json({
+			age. USERS.getYoungest()
+		});
+	}
+	findByOccupation(req, res){
+		res.json({
+			user: USERS.findByOccupation(req.occupation)
+		});
+	}
+	findByGender(req, res){
+		res.json({
+			user: USERS.findByGender(req,query.gender || req.body.gender)
+		});
+	}
+}
